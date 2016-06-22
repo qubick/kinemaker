d830f71cc7a72d0287e18203649afb4b26ac7929
qubick <qubick.kim@gmail.com>
2016-06-22 16:46:36 -0400

    serve site in browser by server

--- simulator/server.js
+++ simulator/server.js
@@ -1,5 +1,3 @@
-// var cam = require("./cam.js")
-// var http = require('http')
 var THREE   = require('three');
 var ejs     = require('ejs');
 var express = require('express');
@@ -22,31 +20,5 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000);
-
-
-// const PORT = 3000
-//
-// function handleRequest(req, res){
-// 	console.log(req.method)
-//
-// 	if(req.method == "POST"){
-// 		console.log('post')
-// 		res.end('eneded up post')
-// 	}
-//
-// 	if(req.method == "GET"){
-// 		console.log('get')
-//
-// 		/*req.on('data', function(data){
-//
-// 		})*/
-// 		res.end('endded up get')
-// 	}
-// }
-//
-// var server = http.createServer(handleRequest)
-// server.listen(PORT, function(){
-// 	console.log("Server listening on: http://localhost: %s", PORT)
-// })
 
 module.exports = app
